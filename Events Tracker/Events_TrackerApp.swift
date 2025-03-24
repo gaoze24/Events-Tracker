52238a6bedea9ed1b514d348dd6d636d75b98843
Gao Ze <eddiegaoze@gmail.com>
2025-03-24 23:53:29 +0800

    Initial Commit

---
+++ Events Tracker/Events_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Events_TrackerApp.swift
+//  Events Tracker
+//
+//  Created by Eddie Gao on 24/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Events_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
